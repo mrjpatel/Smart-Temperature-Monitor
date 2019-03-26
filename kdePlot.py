6ba9ee94a6b419fb6703d0e8fc52cb39bd786a88
rjmenezes <s3600950@student.rmit.edu.au>
2019-03-26 21:22:39 +1100

    WIP: Added KDEPlot module

---
+++ kdePlot.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+
+
+class KDEPlot:
+    @staticmethod
+    def plot_and_save(x_data_set, y_data_set, output_file_name):
+        graph = sns.kdeplot(x_data_set, y_data_set, shade=True)
+        figure = graph.get_figure()
+        figure.savefig(output_file_name, dpi=400)
