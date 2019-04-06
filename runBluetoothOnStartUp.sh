8f7580abbbfc846a6d7b33c38583955086b2d855
rjmenezes <s3600950@student.rmit.edu.au>
2019-04-06 19:35:05 +1100

    Fix: systemd service template

--- runBluetoothOnStartUp.sh
+++ runBluetoothOnStartUp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sed -i "s+/home/pi/sample+$(pwd)/greenhouse_bluetooth+g" bluetooth-notify.service
+sed -i "s+/home/pi/+$(pwd)/+g" bluetooth-notify.service
+sed -i "s+sample+greenhouse_bluetooth+g" bluetooth-notify.service
 sudo cp $(pwd)/bluetooth-notify.service /lib/systemd/system/
 sudo chmod 664 /lib/systemd/system/bluetooth-notify.service
 sudo systemctl daemon-reload
