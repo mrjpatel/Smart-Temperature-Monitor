ea90b86f61e60bff087465d928aea0e88dbdcda4
rjmenezes <s3600950@student.rmit.edu.au>
2019-04-06 15:00:20 +1100

    Updated bluetooth code

--- greenhouse_bluetooth.py
+++ greenhouse_bluetooth.py
@@ -62,8 +62,6 @@ class Bluetooth_notify:
     def is_in_range(paired_list):
         print("Scanning...")
         nearby_devices = bluetooth.discover_devices()
-        for macAddress in nearby_devices:
-            print("Found device with mac-address: " + macAddress)
         print("Nearby: ")
         print(*nearby_devices, sep=", ")
 
