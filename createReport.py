4c317a91c2a8657eb040c7d7cfa6f5f71dd31e43
Japan Patel <japanpatel1198@gmail.com>
2019-04-07 12:55:40 +1000

    Refactored generate day status function

--- createReport.py
+++ createReport.py
@@ -78,23 +78,20 @@ class CreateReport:
             hum_min_value = min(humidity)
             min_reading = ClimateReading(date, temp_min_value, hum_min_value)
             min_error = min_reading.outside_config_range(range)
-            if min_error.startswith("Temp") or min_error.startswith("Hum"):
-                min_error = "BAD: " + min_error
 
             if (temp_max_value != temp_min_value or
                     hum_min_value != hum_max_value):
                 max_reading = ClimateReading(
                     date, temp_max_value, hum_max_value)
                 max_error = max_reading.outside_config_range(range)
-                if max_error.startswith("Temp") or max_error.startswith("Hum"):
-                    max_error = "BAD: " + max_error
 
-        if max_error.startswith("BAD"):
-            error = min_error + max_error.split("BAD: ", 1)[1]
+        if max_error.startswith("Temp") and min_error.startswith("Hum"):
+            error = (max_error + min_error).strip()
         else:
-            error = min_error + max_error
-        if error.startswith("BAD"):
-            return error
+            error = (min_error + max_error).strip()
+
+        if error.startswith("Temp") or error.startswith("Hum"):
+            return "BAD: " + error
         else:
             return "OK"
 
